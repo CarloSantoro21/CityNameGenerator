06ca88c163d1f314d057bc2d3c46839b8c1cdcc7
CarloSantoro21 <alessandrosantoro212121@gmail.com>
2024-08-22 14:45:39 -0600

    add the arrays of strings

---
+++ modules/data.js
@@ -0,0 +1,40 @@
+const first = [
+  "The next time ",
+  "In the future, ",
+  "Very soon, ",
+  "Before long, ",
+  "Fate will lead you to ",
+  "You will soon discover that ",
+  "In the near future, ",
+  "When you least expect it, ",
+  "The next chapter of your life ",
+  "The opportunity to "
+]
+
+const second = [
+  "a great change",
+  "new adventures",
+  "an unexpected surprise",
+  "an unforeseen path",
+  "great opportunities",
+  "an exciting challenge",
+  "a clear goal",
+  "a crucial decision",
+  "an eye-opening experience",
+  "a challenging achievement"
+]
+
+const third = [
+  " will lead you to success.",
+  " will open doors for you.",
+  " will change your perspective.",
+  " will transform your life.",
+  " will make you stronger.",
+  " will guide you to happiness.",
+  " will bring you clarity.",
+  " will be revealing.",
+  " will push you forward.",
+  " will lead you to fulfill your dreams."
+]
+
+export { first, second, third }
